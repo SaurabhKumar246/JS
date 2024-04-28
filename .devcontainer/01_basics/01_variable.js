0aaec0df682e57158bcc8e60a08f2c0089242b98
SaurabhKumar246 <85639390+SaurabhKumar246@users.noreply.github.com>
2024-04-28 05:11:14 +0000

    datatypes

---
+++ .devcontainer/01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 125541
+let accountEmail = "saurabhrajgupta587@gmail.com"
+var accountPassword = "12345"
+accountCity = "Patna"
+
+accountEmail = "saurabh.kumar.cs.2019@mitmeerut.ac.in"
+accountPassword = "11111"
+accountCity = "Delhi"
+
+let accountState;
+
+console.log(accountId)
+
+console.table([accountEmail, accountPassword, accountCity, accountState])
---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+// code readablity should be high
+
+let name = "Saurabh"
+let age = 23
+let isLoggedIn = false
+
+// number => 2 to power 53
+//bigint if number is too large
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique 
+
+// object => null
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined

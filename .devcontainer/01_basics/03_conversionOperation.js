5440d97767692ba18d9979a3392f45eab0e98000
SaurabhKumar246 <85639390+SaurabhKumar246@users.noreply.github.com>
2024-04-28 06:25:29 +0000

    increament and decreament

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -15,7 +15,7 @@ let valueNumber = Number(score)
 let isLoggedIn = "saurabh"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -24,5 +24,51 @@ console.log(booleanIsLoggedIn);
 let someNumber = 44
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//********* Operations ************
+
+let value = 5
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " saurabh"
+
+let str3 = str1 + str2
+//console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(3 + 4 * 5 % 3);
+
+//console.log(true);
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2 //not prefer to code like this
+
+// let gameCounter = 100
+// ++gameCounter;
+// console.log(gameCounter);
+
+let x = 5;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+
+let a = 2;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);

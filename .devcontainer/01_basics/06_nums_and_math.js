45bf10fc16ddf1a1a84b4347a7cf2b2aebe6e75b
SaurabhKumar246 <85639390+SaurabhKumar246@users.noreply.github.com>
2024-05-12 16:35:28 +0000

    number_and_math

---
+++ .devcontainer/01_basics/06_nums_and_math.js
@@ -0,0 +1,11 @@
+const score = 400
+
+const balance = new Number(100)
+console.log(balance);
+console.log(score);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2))
+
+const otherNumber = 23.8966
+console.log(otherNumber.toPrecision(3))
